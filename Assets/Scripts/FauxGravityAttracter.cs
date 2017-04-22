ffb885a8c82edbc6d13d37dd51cadfe1781f794b
Josh <Josh>
2017-04-21 22:00:23 -0400

    Got Player And Gravity

---
+++ Assets/Scripts/FauxGravityAttracter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityAttracter : MonoBehaviour {
+
+    public float gravity;
+
+    public void Attract(Rigidbody rb) {
+        Vector3 gravityUp = (rb.position - transform.position).normalized;
+        Vector3 localUp = rb.transform.up;
+
+        rb.AddForce(gravityUp * gravity);
+
+        rb.rotation = Quaternion.FromToRotation(localUp, gravityUp) * rb.rotation;
+    }
+}
---
+++ Assets/Scripts/FauxGravityBody.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class FauxGravityBody : MonoBehaviour {
+
+    // Use this for initialization
+    FauxGravityAttracter planet;
+    Rigidbody body;
+	void Awake () {
+        planet = GameObject.FindGameObjectWithTag("Planet").GetComponent<FauxGravityAttracter>();
+        body = GetComponent<Rigidbody>();
+
+        body.useGravity = false;
+        body.constraints = RigidbodyConstraints.FreezeRotation;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        planet.Attract(body);
+	}
+}
