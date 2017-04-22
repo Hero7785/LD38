17fd76cddacca2566f20101e216f8611e678fdde
Josh <Josh>
2017-04-22 10:46:49 -0400

    Made First Level

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+
+    public float timeLeft;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft < 0) {
+            Debug.Log("Die");
+        }
+	}
+}
